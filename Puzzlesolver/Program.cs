672e43d2db7e7529f02b5e9a4b1cd5bc89e9cbe3
Hoang <phuongtr.hoang@mm-bbs.de>
2024-08-28 14:35:06 +0200

    Add connection to MongoDB Server

--- Puzzlesolver/Program.cs
+++ Puzzlesolver/Program.cs
@@ -1,7 +1,18 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory) // Set the base path for appsettings.json
+    .AddJsonFile("appsettings.json") // Add the appsettings.json file
+    .Build();
+
+var mongoClient = new MongoClient(configuration.GetConnectionString("MongoDb"));//set connection string
+builder.Services.AddSingleton<IMongoClient>(mongoClient);
+
 
 var app = builder.Build();
 
@@ -13,12 +24,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
