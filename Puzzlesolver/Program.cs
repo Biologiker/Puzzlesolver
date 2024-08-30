fea8c48cf1b1178fda61688e78e0cce68426460a
Hoang <phuongtr.hoang@mm-bbs.de>
2024-08-30 11:14:42 +0200

    Change connection type

--- Puzzlesolver/Program.cs
+++ Puzzlesolver/Program.cs
@@ -1,4 +1,24 @@
 using MongoDB.Driver;
+using MongoDB.Bson;
+
+const string connectionUri = "mongodb+srv://application:yXHdOt14pJBBx4vS@puzzlesolver.x62el.mongodb.net/?retryWrites=true&w=majority&appName=Puzzlesolver";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+// Send a ping to confirm a successful connection
+try
+{
+    var result = client.GetDatabase("Puzzlesolver").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    //client.ListDatabaseNames(default);
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
 
 var builder = WebApplication.CreateBuilder(args);
 
