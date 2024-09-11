d8fb488128266b37211d0002e07fba521c3b3855
Hoang <phuongtr.hoang@mm-bbs.de>
2024-09-11 14:20:01 +0200

    WIP: add crossword solving algorithm

---
+++ CrosswordSolver/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CrosswordSolver;
+
+namespace CrosswordSolver
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            char[,] grid = {
+                { ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ' },
+                { ' ', ' ', ' ', ' ', ' ' }
+            };
+
+            var words = new List<string> { "apple", "pear", "dog" };
+
+            var solver = new CrosswordSolver();
+
+            try
+            {
+                var solvedGrid = solver.Solve(words, grid);
+                solver.PrintGrid(solvedGrid); // Output the solved grid
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+}
